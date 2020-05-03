f58b6a8bd8fceb1388d4ea67d5333708bb74da37
Denis Tatar <denis.tatar8@gmail.com>
2020-05-03 05:41:04 -0400

    Added Data view controller

---
+++ NutriFacts-App/NutriFit/DataViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DataViewController.swift
+//  NutriFacts
+//
+//  Created by Denis Tatar on 2020-05-03.
+//  Copyright © 2020 Denis Tatar. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
---
+++ NutriFacts-App/NutriFit/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  NutriFacts
+//
+//  Created by Denis Tatar on 2020-05-03.
+//  Copyright © 2020 Denis Tatar. All rights reserved.
+//
+
+import Foundation
