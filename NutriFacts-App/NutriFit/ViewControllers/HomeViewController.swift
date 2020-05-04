0205cdcab4ef9ca4e82a27c15ed2146f134450d3
Iris Chen <irischenlj@gmail.com>
2020-05-04 15:11:27 -0400

    parse scanned facts

--- NutriFacts-App/NutriFit/ViewControllers/HomeViewController.swift
+++ NutriFacts-App/NutriFit/ViewControllers/HomeViewController.swift
@@ -10,6 +10,8 @@ class HomeViewController: UIViewController, VNDocumentCameraViewControllerDelega
     
     // constants for the view controller
     let textRecognitionWorkQueue = DispatchQueue(label: "TextRecognitionQueue", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
+    // global variable for everything scanned
+//    let detectedText = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,18 +59,65 @@ class HomeViewController: UIViewController, VNDocumentCameraViewControllerDelega
     // PARSING IMAGES
     var textRecognitionRequest = VNRecognizeTextRequest { (request, error) in
         guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
-        var detectedText = ""
-
+        var local_detected_text = ""
         for observation in observations {
             guard let topCandidate = observation.topCandidates(1).first else { return }
 
             // string to contain parsed data
-            detectedText += topCandidate.string
-            detectedText += "\n"
-            print("New Detected Material:")
-            print(detectedText)
+            local_detected_text += topCandidate.string
+            local_detected_text += "\n"
         }
+        // the last detected text is the one you want
+        // fat, cholesterol, sodium, carbohydrate, protein, and iron
+        let detected_array = local_detected_text.components(separatedBy: "\n")
+        let default_intake = "0%"
+        var nutrient_dict =
+        [
+            "Fat" : default_intake,
+            "Cholesterol" : default_intake,
+            "Sodium": default_intake,
+            "Carbohydrate": default_intake,
+            "Protein": default_intake,
+            "Iron": default_intake
+        ]
+        
+        for (index, line) in detected_array.enumerated() {
+
+            if (line.contains("Fat")) {
+                var percentage_intake = detected_array[index+1]
+                if (percentage_intake.contains("%")) {
+                    nutrient_dict["Fat"] = percentage_intake
+                }
+            } else if (line.contains("Cholesterol")) {
+                var percentage_intake = detected_array[index+1]
+                if (percentage_intake.contains("%")) {
+                    nutrient_dict["Cholesterol"] = percentage_intake
+                }
+            } else if (line.contains("Sodium")) {
+                var percentage_intake = detected_array[index+1]
+                if (percentage_intake.contains("%")) {
+                    nutrient_dict["Sodium"] = percentage_intake
+                }
+            } else if (line.contains("Carbohydrate")) {
+                var percentage_intake = detected_array[index+1]
+                if (percentage_intake.contains("%")) {
+                    nutrient_dict["Carbohydrate"] = percentage_intake
+                }
+            } else if (line.contains("Protein")) {
+                var percentage_intake = detected_array[index+1]
+                if (percentage_intake.contains("%")) {
+                    nutrient_dict["Protein"] = percentage_intake
+                }
+            } else if (line.contains("Iron")) {
+                var percentage_intake = detected_array[index+1]
+                if (percentage_intake.contains("%")) {
+                    nutrient_dict["Iron"] = percentage_intake
+                }
+            }
+        }
+//    dump(nutrient_dict)
     }
+    
     @IBOutlet weak var viewScanButton: UIButton!
     
     // handler for parsing
@@ -79,6 +128,7 @@ class HomeViewController: UIViewController, VNDocumentCameraViewControllerDelega
             let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
             do {
                 try requestHandler.perform([self.textRecognitionRequest])
+//                print(detectedText)
             } catch {
                 // lolll
                 print(error)
